c60bb50fbf9a6bfe6c554d1b3cc85f20fed5c802
fanni-create <efani.nih@gmail.com>
2026-06-02 11:42:43 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { useState, useMemo, useRef, useEffect } from "react";
-
+ 
 // Load Montserrat font
 const fontLink = document.createElement("link");
 fontLink.href = "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap";
